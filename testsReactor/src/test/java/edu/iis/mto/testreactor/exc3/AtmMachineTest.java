342dc1484b4bc2911549f2618a006fe5fe60a875
Adam Sylla <sylla.adam.97@gmail.com>
2019-05-16 16:51:47 +0200

    *added atmMachineShouldReturnAppropriateBankntos test

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
@@ -3,9 +3,64 @@ package edu.iis.mto.testreactor.exc3;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class AtmMachineTest {
+
+    AtmMachine atmMachine;
+
+    //data
+    Money amount;
+    Card card;
+    AuthenticationToken authenticationToken;
+
+    Optional<AuthenticationToken> token;
+
+    //mocks
+    CardProviderService cardProviderService;
+    BankService bankService;
+    MoneyDepot moneyDepot;
+
+    @Before
+    public void setup(){
+        cardProviderService = Mockito.mock(CardProviderService.class);
+        bankService = Mockito.mock(BankService.class);
+        moneyDepot = Mockito.mock(MoneyDepot.class);
+    }
+
+    @Test
+    public void atmMachineShouldReturnAppropriateBankntos(){
+        authenticationToken = AuthenticationToken.builder().withUserId("1").withAuthorizationCode(1234).build();
+        token = Optional.of(authenticationToken);
+
+        amount = Money.builder().withAmount(120).withCurrency(Currency.PL).build();
+        card = Card.builder().withCardNumber("123").withPinNumber(1234).build();
+
+        atmMachine = new AtmMachine(cardProviderService,bankService,moneyDepot);
+
+        Mockito.when(cardProviderService.authorize(card)).thenReturn(token);
+
+        Mockito.when(bankService.charge(authenticationToken,amount)).thenReturn(true);
+
+        Mockito.when(moneyDepot.releaseBanknotes(Mockito.any())).thenReturn(true);
+
+        Payment paymentFromAtmMachine = atmMachine.withdraw(amount,card);
+
+        List<Banknote> banknoteList = new ArrayList<>();
+        banknoteList.add(Banknote.PL100);
+        banknoteList.add(Banknote.PL20);
+
+        Payment expectedPayment = new Payment(banknoteList);
+
+        Assert.assertEquals(expectedPayment,paymentFromAtmMachine);
+    }
 
     @Test
     public void itCompiles() {
